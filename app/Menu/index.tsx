fe10616652d847e955a2e31a189c4e9f879e1c51
DanielForero14 <danielforsa@unisabana.edu.co>
2025-03-31 14:25:00 -0500

    "feat(index): agregar implementación de cámara en la página principal"

--- app/Menu/index.tsx
+++ app/Menu/index.tsx
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, TextInput, Touchable, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import Entypo from '@expo/vector-icons/Entypo';
 
 export default function index() {
-    const [image, setImage] = useState(undefined as any);
+  const [image, setImage] = useState(undefined as any);
 
   return (
-    <View>
-        
+    <View
+      style={{
+        flex: 1,
+        padding: 20,
+      }}
+    >
+      {/*Select Image */}
+      {image ? <View>
+        {/* Pintar la imagen */}
+      </View> 
+      : 
+      <TouchableOpacity
+      onPress={()=>()}
+      >
+      <Entypo name="camera" size={24} color="black" />
+      </TouchableOpacity>
+       }
+
+      <view></view>
+      {/* Title */}
+
+      <TextInput />
+      {/* Prince */}
+      <TextInput />
+      {/* Description */}
+      <TextInput />
+
+      {/* Los tres botones que debe tener son */}
+      {/* Edid */}
+      {/* Delete */}
+      {/*Select Image */}
     </View>
-  )
+  );
 }
