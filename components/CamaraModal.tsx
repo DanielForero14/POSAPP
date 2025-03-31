4c66c3b74f706272e1103c459823e9f2b67d6d7c
DanielForero14 <danielforsa@unisabana.edu.co>
2025-03-31 15:02:59 -0500

    Modal camera buttons

--- components/CamaraModal.tsx
+++ components/CamaraModal.tsx
@@ -1,15 +1,74 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { View, Text, Modal, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
-interface CameraModalProps{
+interface CameraModalProps {
     isVisible: boolean
     image?: any;
 }
 export default function CamaraModal(props: CameraModalProps) {
+    const [facing, setFacing] = useState<CameraType>('back');
+    const [permission, requestPermission] = useCameraPermissions();
 
-  return (
-    <View>
-      <Text>CamaraModal</Text>
-    </View>
-  )
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <View />;
+    }
+
+    if (!permission.granted) {
+        // Camera permissions are not granted yet.
+        return (
+            <View style={{
+                flex: 1
+            }}>
+                <Text>We need your permission to show the camera</Text>
+            </View>
+        );
+    }
+
+    return (
+        <Modal
+            visible={props.isVisible}
+        >
+            <View
+                style={{
+                    flex: 1
+                }}
+            >
+                <CameraView style={{
+                    flex: 1
+                }}
+                    facing={facing}
+                >
+                    <View style={{
+                        flexDirection:"row"
+                    }}>
+                        <TouchableOpacity
+                        onPress={take}
+                        >
+                            <Text> Take a Foto</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                        onPress={Open}
+                        >
+                            <Text>Open library</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                        onPress={flip}
+                        >
+                            <Text>Flip camera</Text>
+                        </TouchableOpacity>
+                        {/*Take a foto*/}
+                        {/*Open Library */}
+                        {/*Flip Camera*/}
+
+
+                        
+                    </View>
+
+                </CameraView>
+            </View>
+
+        </Modal>
+    )
 }
